b1a3a6b930034cdf5a894bfed89a4c2289d6ec55
Matheus Cardoso <dacachmatheus@gmail.com>
2023-03-02 21:21:49 -0300

    Parse CSV rows into tuples

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,3 +1,4 @@
+import org.apache.spark.rdd.RDD
 import org.apache.spark.sql.SparkSession
 
 object Main {
@@ -7,5 +8,16 @@ object Main {
 
     val inputDirectory = "input"
     val inputRDD = sc.textFile(inputDirectory)
+
+    val parsedRDD = parseIntoTuple(inputRDD)
+  }
+
+  private def parseIntoTuple(inputRDD: RDD[String]): RDD[(Int, String, Int)] = {
+    inputRDD.map(line => {
+      val Array(numberStr, company, valueStr) = line.split(',')
+      val number = numberStr.toInt
+      val value = valueStr.toInt
+      (number, company, value)
+    })
   }
 }
